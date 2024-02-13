6929c1f54cdf7e6b87d95d61f309f5a334ae559d
ncicka <ncicka@gmail.com>
2024-02-13 20:35:25 -0300

    feat: integrar el wallet-adapter

--- reto1/src/app/app.component.ts
+++ reto1/src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'reto1-root',
   template: `
     <header class="px-16 pt-24 pb-8">
       <h1 class="text-center text-5xl mb-4">My Bank</h1>
-
+      <div class="flex justify-center">
+        <hd-wallet-multi-button></hd-wallet-multi-button>
+      </div>
     </header>
 
     <main></main>
   `,
 })
-export class AppComponent {
-}
+export class AppComponent {}
