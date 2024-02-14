cf09d0dea6f958e98e4ae458f2a8440f924303f8
ncicka <ncicka@gmail.com>
2024-02-13 22:38:10 -0300

    feat: mostrar balance

--- reto1/src/app/app.component.ts
+++ reto1/src/app/app.component.ts
@@ -1,20 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterModule } from '@angular/router';
+import { WalletStore } from '@heavy-duty/wallet-adapter';
 import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
+import { computedAsync } from 'ngxtension/computed-async';
+import { ShyftApiService } from './shyft-api.service';
 
 @Component({
   standalone: true,
   imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'reto1-root',
   template: `
-    <header class="px-16 pt-24 pb-8">
+    <header class="py-8 relative">
       <h1 class="text-center text-5xl mb-4">My Bank</h1>
       <div class="flex justify-center">
         <hd-wallet-multi-button></hd-wallet-multi-button>
       </div>
+      @if (account()) {
+        <div
+          class="absolute top-4 left-4 flex justify-center items-center gap-2"
+        >
+          <img [src]="account()?.info?.image" class="w-8 h8" />
+          <p class="text-xl">{{ account()?.balance }}</p>
+        </div>
+      }
     </header>
 
     <main></main>
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  private readonly _shyftApiService = inject(ShyftApiService);
+  private readonly _walletStore = inject(WalletStore);
+  private readonly _publicKey = toSignal(this._walletStore.publicKey$);
+
+  readonly account = computedAsync(
+    () => this._shyftApiService.getAccount(this._publicKey()?.toBase58()),
+    { requireSync: true },
+  );
+}
