cdebb1eeb69326edecfb32d9065c36cf80350d0c
ncicka <ncicka@gmail.com>
2024-02-20 19:08:21 -0300

    chore: estetica visual

--- reto1/src/app/features-section.component.ts
+++ reto1/src/app/features-section.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'reto1-features-section',
   template: `
-    <section class="px-16 py-4">
+    <section class="px-16 py-24 bg-white bg-opacity-5">
       <ul class="flex justify-center items-center gap-16">
         <li class="text-xl font-bold">Rapido</li>
         <li class="text-xl font-bold">Eficiente</li>
